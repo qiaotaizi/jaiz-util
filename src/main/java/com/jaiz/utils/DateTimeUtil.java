bfcb6abb700ef4d4f9cfa5e642c4de09c9ff8ae0
zhiheng.jiang <zhiheng.jiang@ttpai.cn>
2019-02-26 18:31:59 +0800

    pom升级

--- src/main/java/com/jaiz/utils/DateTimeUtil.java
+++ src/main/java/com/jaiz/utils/DateTimeUtil.java
@@ -1,6 +1,7 @@
 package com.jaiz.utils;
 
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * 日期,时间工具类
@@ -26,5 +27,61 @@ public class DateTimeUtil {
         dayEnd.set(Calendar.MINUTE,59);
         dayEnd.set(Calendar.SECOND,59);
         return dayEnd.getTimeInMillis()/1000l-current;
+    }
+
+    /**
+     * 获取当前月份的第一天
+     * @return
+     */
+    public static final Date firstDayOfThisMonth(){
+        Calendar calendar=Calendar.getInstance();
+        calendar.set(Calendar.SECOND,0);
+        calendar.set(Calendar.MINUTE,0);
+        calendar.set(Calendar.HOUR_OF_DAY,0);
+        calendar.set(Calendar.DAY_OF_MONTH,1);
+        return calendar.getTime();
+    }
+
+    /**
+     * 获取下个月的第一天
+     * @return
+     */
+    public static final Date firstDayOfNextMonth(){
+        Calendar calendar=Calendar.getInstance();
+        calendar.add(Calendar.MONTH,1);
+        calendar.set(Calendar.SECOND,0);
+        calendar.set(Calendar.MINUTE,0);
+        calendar.set(Calendar.HOUR_OF_DAY,0);
+        calendar.set(Calendar.DAY_OF_MONTH,1);
+        return calendar.getTime();
+    }
+
+    /**
+     * 判断给定时间戳是否是周末
+     * @param ts
+     * @return
+     */
+    public static boolean isWeekend(long ts) {
+        return isWeekend(new Date(ts));
+    }
+
+    /**
+     * 判断给定时间戳是否是周末
+     * @param d
+     * @return
+     */
+    public static boolean isWeekend(Date d){
+        Calendar c=Calendar.getInstance();
+        c.setTime(d);
+        return isWeekend(c);
+    }
+
+    /**
+     * 判断给定时间戳是否是周末
+     * @param c
+     * @return
+     */
+    public static boolean isWeekend(Calendar c){
+        return c.get(Calendar.DAY_OF_WEEK)==Calendar.SATURDAY || c.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY;
     }
 }
